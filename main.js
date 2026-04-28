5afb9c8011158840502db830ca1d6b5dca247096
shubh4268 <shubham@mediavisioninfo.com>
2026-04-28 11:18:33 +0530

    add audio and play pause feature

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
-onload = () =>{
-    document.body.classList.remove("container");
+console.log("JS loaded");
+onload = () => {
+  document.body.classList.remove("container");
 };
+
+const audio = document.querySelector("audio");
+
+window.addEventListener("click", () => {
+  console.log("User clicked");
+
+  if (audio.paused) {
+    audio
+      .play()
+      .then(() => console.log("Playing audio"))
+      .catch((err) => console.log("Error:", err));
+  } else {
+    audio.pause();
+    console.log("Audio paused");
+  }
+});
